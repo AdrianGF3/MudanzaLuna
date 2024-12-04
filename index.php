46c86e19338fe913ea6d9f88f60161df4783ec0d
AdrianGF3 <adrian.gofa@gmail.com>
2024-12-04 13:02:03 +0100

    se implementaron imagenes, secciones, el menu de navegación y se implementó un query para manejo de imagenes

--- index.php
+++ index.php
@@ -4,11 +4,45 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Mudanzas Luna</title>
+    <link rel="stylesheet" href="css/nivo-slider.css">
+    <link rel="stylesheet" href="themes/default/default.css">
+    <script src="js/jquery-3.6.0.min.js"></script>
+    <script src="js/jquery.nivo.slider.js"></script>
+    <script src="js/jsPlugins.js"></script>
+
 </head>
 
 <body>
-
+    <header>
+        <h1><img src="index.php" alt="MudanzasLuna" class="logo"></h1>
+        <nav>
+            <ul>
+                <li><a href="" class="navMenu">Inicio</a></li>
+                <li><a href="" class="navMenu">Servicios</a></li>
+                <li><a href="" class="navMenu">Nosotros</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section id="inicio">
+            <div class="container">
+                <h2>Bienvenido a Mudanzas Luna!</h2>
+                <p>Somos tu mejor opción para mudanzas rápidas, seguras y económicas. Nos encargamos de llevar tus cosas a donde necesites, cuidando cada detalle.</p>
+                <a href="#contacto" class="btn-primary">Solicita tu mudanza ahora</a>
+            </div>
+        </section>
+        <section class="imgSlider">
+            <div id="slider" class="nivoSlider">
+                <img src="img/camionMudanzas.jpg" data-thumb="img/camionMudanzas.jpg" alt="" />
+                <img src="img/camionMudanzas2.jpg" data-thumb="img/camionMudanzas2.jpg" alt="" title="This is an example of a caption" />
+                <img src="img/camionMudanzas3.PNG" data-thumb="img/camionMudanzas3.PNG" alt="" data-transition="slideInLeft" />
+            </div>
+        </section>
+    </main>
+    <footer>
+        <div class="mensajeFinal">todos los derechos reservados</div>
+    </footer>
 </body>
 
 </html>
