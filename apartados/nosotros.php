9e3b22ac2c102d844be686afdaf51ca1bf19963e
AdrianGF3 <adrian.gofa@gmail.com>
2024-12-05 12:35:32 +0100

    se empezó el apartado "nosotros"

---
+++ apartados/nosotros.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>nosotros</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="" class="navMenu">Inicio</a></li>
+                <li><a href="" class="navMenu">Servicios</a></li>
+                <li><a href="" class="navMenu">Nosotros</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section id="inicio">
+            <div class="container">
+                <h2>Bienvenido a Mudanzas Luna!</h2>
+                <p>Somos tu mejor opción para mudanzas rápidas, seguras y económicas. Nos encargamos de llevar tus cosas a donde necesites, cuidando cada detalle.</p>
+            </div>
+        </section>
+        <section class="FAQ1">
+            <div class="PreguntaBloqueTitulo">
+                <h1>Preguntas sobre mudanzas</h1>
+            </div>
+            <div class="PreguntaBloqueTexto">
+                <p>En Mudanzas Luna resolvemos todas las dudas que te puedan surgir sobre el funcionamiento de nuestras mudanzas, guardamuebles, alquiler de plataformas elevadoras e información sobre nuestros seguros, ante cualquier situación.</p>
+            </div>
+        </section>
+        <section class="FAQ2">
+            <article class="DesplegablePregunta">
+                <a href="" id="TituloPregunta"><h3>¿En qué nos basamos para calcular el precio de una mudanza?</h3></a>
+                <div class="DesplegadoFAQ">
+                    <p>Valoramos, entre otros, los siguientes datos:</p>
+                    <ul>
+                        <li>El <strong>volumen general</strong>, es decir, la cantidad de objetos que hay que trasladar.</li>
+                        <li>El <strong>tipo de objetos y muebles a trasladar</strong>, si requieren desmontaje y posterior montaje en destino.</li>
+                        <li>El <strong>embalaje de los objetos</strong>, es decir, la cantidad de material de embalaje necesario: si lo hace el cliente será más barato.</li>
+                        <li>El <strong>número de operarios</strong> que serán necesarios para una mudanza eficaz que finalice en el tiempo adecuado.</li>
+                        <li>Las <strong>condiciones de acceso a la vivienda</strong>, si hay o no ascensor, si es posible colocar un elevador exterior, comprobación de tramos de escalera y distancia a camión, etc.</li>
+                        <li><strong>Otros aspectos</strong> tales como tipo de vehículo necesario, fechas de ejecución, distancia a destino, etc.</li>
+                    </ul>
+                </div>
+            </article>
+            <article class="DesplegablePregunta">
+                <a href="" id="TituloPregunta"><h3>¿Qué incluye el presupuesto de una mudanza?</h3></a>
+                <div class="DesplegadoFAQ">
+                    <p>Las empresas de mudanzas serias y profesionales, como Mudanzas Luna, incluimos prácticamente todos los servicios que se necesitan para realizar correctamente una mudanza:  Cajas, embalaje y desembalaje, grúa, traslado especial de obras de arte o instrumentos musicales grandes, etc.</p>
+                    <p>Ponte en contacto con nosotros para que podamos darte un precio personalizado para tu mudanza en Barcelona. </p>
+                </div>
+            </article>
+            <article class="DesplegablePregunta">
+                <a href="" id="TituloPregunta"><h3>¿En qué zonas trabaja Mudanzas Luna?</h3></a>
+                <div class="DesplegadoFAQ">
+                    <p>Mudanzas Luna es una empresa de mudanzas y guardamuebles situada en Barcelona, y además de en toda la comunidad autónoma, también realizamos mudanzas a nivel nacional. Puedes pedirnos más información a través del formulario de contacto. </p>
+                </div>
+            </article>
+            <article class="DesplegablePregunta">
+                <a href="" id="TituloPregunta"><h3>¿Cómo pedir presupuesto para una mudanza?</h3></a>
+                <div class="DesplegadoFAQ">
+                    <p>Puedes contactar con Mudanzas Calleja de varias formas:
+                    <ul>
+                        <li>Rellenando el formulario de solicitud de presupuestos</li>
+                        <li>Enviando un email a info@mudanzascalleja.com</li>
+                        <li>Llamando al 91 388 01 44</li>
+                        <li>De manera presencial en nuestra sede en la Calle Hermanos Gascón, 74 – Madrid</li>
+                    </ul>
+                    <p>Los datos básicos que necesitaremos para presupuestar cualquier tipo mudanza son: Tus datos de contacto, fecha aproximada en la que necesitas la mudanza y origen y destino.</p>
+                    <p>¡Tenlos claros para que te podamos dar una respuesta lo más pronto posible!</p>
+                </div>
+            </article>
+            <article class="DesplegablePregunta">
+                <a href="" id="TituloPregunta"><h3>¿Cuánto cuesta una mudanza?</h3></a>
+                <div class="DesplegadoFAQ">
+                    <p>Los factores que influyen en el precio de una mudanza son muy variados ya que partimos de la base de que no hay dos viviendas iguales, por lo tanto, no hay dos mudanzas iguales.</p>
+                    <p>Para valorar adecuadamente el precio una mudanza y ajustarlo lo máximo posible a tus necesidades reales, nuestros técnicos evaluarán detalles como:</p>
+                    <ul>
+                        <li><strong>CANTIDAD Y TIPO DE MUEBLES:</strong> Si requieren desmontaje o montaje, si se necesitará fabricar cajas o emabalajes especiales a medida para cuadros valiosos o esculturas de gran tamaño, etc… ¡Cuánto más nos cuentes mejor podremos adaptar el precio de tu mudanza!</li>
+                        <li><strong>CONTENIDOS:</strong> Cantidad y tipo de material de embalaje necesario, es decir, el número estimado de cajas necesarias para embalar contenidos como los enseres personales, ropa, adornos, vajillas, libros, etc. Si ya has embalado todo tú mismo y lo tienes todo listo, el precio será todavía más económico.</li>
+                        <li><strong>ACCESOS:</strong> Domicilio de carga (condiciones de acceso, si dispone o no de ascensor, zona de aparcamiento regulado, si requiere de permisos especiales o colocación de placas, etc). También se comprueba la posibilidad de colocar un elevador monta-muebles exterior siempre que sea posible y conveniente con todas las ventajas que ello conlleva como por ejemplo facilitar la carga o descarga, evitar el uso de escaleras o ascensores, rapidez y eficacia en la mudanza, etc.</li>
+                        <li><strong>DISTANCIA:</strong> En distancias cortas como pueda ser una mudanza en Madrid Capital o Madrid provincia, normalmente no influye el kilometraje o lo hace muy poco. Sin embargo, una mudanza por ejemplo a Málaga o a Barcelona sí que influirá la distancia debido al kilometraje que hay que recorrer y el tiempo de tránsito.</li>
+                    </ul>
+                    <p>Por eso, nuestros técnicos evalúan y te asesoran sobre cuál sería la forma óptima de planificar la mudanza sea cual sea tu caso.</p>
+                </div>
+            </article>
+        </section>
+    </main>
+    <footer>
+        <div class="mensajeFinal">todos los derechos reservados</div>
+    </footer>
+</body>
+</html>
