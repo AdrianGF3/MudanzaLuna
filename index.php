af2521a3c0adbfa2ca62a6baf38258d0f400ab01
kianMIC <ney_apolinario1@hotmail.com>
2024-12-05 12:19:31 +0100

    implementacion del tema para la Slider, e implementacion de paleta de colores para mejor resolucion de los estilos a futuro

--- index.php
+++ index.php
@@ -5,8 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mudanzas Luna</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="css/nivo-slider.css">
-    <link rel="stylesheet" href="themes/default/default.css">
+    <link rel="stylesheet" href="theme/default/default.css">
     <script src="js/jquery-3.6.0.min.js"></script>
     <script src="js/jquery.nivo.slider.js"></script>
     <script src="js/jsPlugins.js"></script>
@@ -24,22 +26,26 @@
             </ul>
         </nav>
     </header>
-    <main>
+    <div id="wrapper">
+        <main>
         <section id="inicio">
             <div class="container">
-                <h2>Bienvenido a Mudanzas Luna!</h2>
-                <p>Somos tu mejor opción para mudanzas rápidas, seguras y económicas. Nos encargamos de llevar tus cosas a donde necesites, cuidando cada detalle.</p>
+                <h2>¡Bienvenidos a Mudanzas Luna! 🌙</h2>
+                <p>Tu tranquilidad es nuestra prioridad. Sabemos lo importante que son tus pertenencias, por eso nos encargamos de cada detalle con cuidado, responsabilidad y profesionalismo. 
+                    Ya sea que estés cambiando de casa, oficina o necesites transportar algo especial, en Mudanzas Luna te acompañamos en cada paso del camino. ¡Confía en nosotros y deja tus mudanzas en las mejores manos! 🚚✨
+</p>
             </div>
         </section>
-        <section class="imgSlider">
-            <div id="slider" class="nivoSlider">
-                <img src="img/camionMudanzas.jpg" data-thumb="img/camionMudanzas.jpg" alt="" />
-                <img src="img/camionMudanzas2.jpg" data-thumb="img/camionMudanzas2.jpg" alt="" title="This is an example of a caption" />
-                <img src="img/camionMudanzas3.PNG" data-thumb="img/camionMudanzas3.PNG" alt="" data-transition="slideInLeft" />
+        <section class="SliderTheme">
+            <div id="slider" class="slider-wrap theme-default">
+                <img class="imgSlider" src="img/camionMudanzas.jpg" data-thumb="img/camionMudanzas.jpg" alt="" />
+                <img class="imgSlider" src="img/camionMudanzas2.jpg" data-thumb="img/camionMudanzas2.jpg" alt="" title="" />
+                <img class="imgSlider" src="img/camionMudanzas3.PNG" data-thumb="img/camionMudanzas3.PNG" alt="" data-transition="slideInLeft" />
             </div>
             <a href="#contacto" class="btn-primary">Solicita tu mudanza ahora</a>
         </section>
     </main>
+    </div>
     <footer>
         <div class="mensajeFinal">todos los derechos reservados</div>
     </footer>
